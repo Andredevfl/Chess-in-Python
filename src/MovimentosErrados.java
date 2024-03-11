b8ab02a97057b4bd9a2736cbb48ac15b18caf6eb
João Pedro Bove da Costa <88252025+joao-pbc@users.noreply.github.com>
2024-03-11 15:35:02 -0300

    Wrong movement class
    Added a class to save the wrong movements

---
+++ src/MovimentosErrados.java
@@ -0,0 +1,13 @@
+public class MovimentosErrados{
+
+    int[][] movimentosErrados;
+
+    public MovimentosErrados(int[][] movimentosFeitos){
+        movimentosErrados = movimentosFeitos;
+    }
+
+    public int[][] getMovimentosFeitos() {
+        return movimentosErrados;
+    }
+
+}
